3dc0e5cc2643849d90356b355074a62d678b208e
Connor_Woodard <Connor_Woodard@insideranken.org>
2024-01-22 08:27:53 -0600

    test push

---
+++ CH2LabsSolution/CH2Labs/Models/PriceQuotationModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CH2Labs.Models
+{
+    public class PriceQuotationModel
+    {
+        [Required(ErrorMessage = "Please enter the SubTotal")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a number greater than 0")]
+        public decimal? SubTotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter the Discount Percentage")]
+        [Range(0, 100, ErrorMessage = "Please enter a number between 0 and 100")]
+        public decimal? DiscountPercentage { get; set; }
+
+        public decimal? DiscountAmount { get; private set; }
+        public decimal? Total { get; private set; }
+
+        public void CalculateTotal()
+        {
+            if (SubTotal.HasValue && DiscountPercentage.HasValue)
+            {
+                DiscountAmount = SubTotal.Value * (DiscountPercentage.Value / 100);
+                Total = SubTotal.Value - DiscountAmount;
+            }
+            else
+            {
+                DiscountAmount = Total = 0;
+            }
+        }
+    }
+}
---
+++ CH2LabsSolution/CH2Labs/Models/TipCalculationModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CH2Labs.Models
+{
+    public class TipCalculationModel
+    {
+        [Required(ErrorMessage = "Please enter the cost of the meal")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a number greater than 0")]
+        public decimal? MealCost { get; set; }
+
+        public decimal? SmallTipPercentage { get; } = 0.15m;
+        public decimal? MediumTipPercentage { get; } = 0.20m;
+        public decimal? LargeTipPercentage { get; } = 0.25m;
+
+        public decimal? SmallTipAmount { get; private set; }
+        public decimal? MediumTipAmount { get; private set; }
+        public decimal? LargeTipAmount { get; private set; }
+
+        public void CalculateTipAmount()
+        {
+            if (MealCost.HasValue)
+            {
+                SmallTipAmount = MealCost.Value * SmallTipPercentage;
+                MediumTipAmount = MealCost.Value * MediumTipPercentage;
+                LargeTipAmount = MealCost.Value * LargeTipPercentage;
+            }
+            else
+            {
+                SmallTipAmount = MediumTipAmount = LargeTipAmount = 0;
+            }
+        }
+    }
+}
