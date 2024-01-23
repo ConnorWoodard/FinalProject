ba5fe67364f2935bf587b2c1ae9a227cb19b50aa
Connor_Woodard <Connor_Woodard@insideranken.org>
2024-01-23 09:39:19 -0600

    delete

--- CH2LabsSolution/CH2Labs/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CH2Labs.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
